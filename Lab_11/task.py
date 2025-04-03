18829b82b1ce3a198bf23dc1c608bc257c7217b8
wkwtfigo <e.zagurskih@innopolis.university>
2025-04-03 13:13:47 +0300

    Added full solution to lab 11

--- Lab_11/task.py
+++ Lab_11/task.py
@@ -1,3 +1,4 @@
+import json
 from pymongo import MongoClient
 
 client = MongoClient("mongodb://localhost:27017/")
@@ -10,20 +11,30 @@ def insert_operation(data):
         return f"Inserted single document with ID: {result.inserted_id}"
     elif isinstance(data, list):
         result = collection.insert_many(data)
-        return  f"Inserted single document with ID: {result.inserted_id}"
+        return  f"Inserted single document with ID: {result.inserted_ids}"
     
 def find_operation():
     print("Documents where a coupon was used:")
-    for doc in collection.find({"couponUsed": True}):
-        print(doc)
-    
-    print("\nDocuments with storeLocation in Denver:")
-    for doc in collection.find({"storeLocation": "Denver"}):
+
+    for doc in collection.find({"couponUsed": True}).limit(1):
         print(doc)
 
-    print("\nDocuments where customer satisfaction is less than 4:")
-    for doc in collection.find({"customer.satisfaction": {"$lt": 4}}):
+    count_coupon_used = collection.count_documents({"couponUsed": True})
+    print(f"Total documents where a coupon was used: {count_coupon_used}")
+    
+    print("\nDocuments with storeLocation in Denver:")
+    for doc in collection.find({"storeLocation": "Denver"}).limit(1):
         print(doc)
+
+    count_denver = collection.count_documents({"storeLocation": "Denver"})
+    print(f"Total documents with storeLocation in Denver: {count_denver}")
+
+    print("\nDocuments where customer satisfaction is less than 4:")
+    for doc in collection.find({"customer.satisfaction": {"$lt": 4}}).limit(1):
+        print(doc)
+
+    count_satisfaction = collection.count_documents({"customer.satisfaction": {"$lt": 4}})
+    print(f"Total documents where customer satisfaction is less than 4: {count_satisfaction}")
 
 def delete_operation():
     collection.delete_one({"_id": "5bd761dcae323e45a93ccff4"})
@@ -36,3 +47,18 @@ def update_operation():
         {"$set": {"customer.satisfaction": 5}}
     )
     print("Updated customer satisfaction to 5.")
+
+
+if __name__ == "__main__":
+    with open('Lab_11\\single_data.json', 'r') as single_file:
+        single_data = json.load(single_file)
+
+    with open('Lab_11\\multiple_data.json', 'r') as multiple_file:
+        multiple_data = json.load(multiple_file)
+    
+    print(insert_operation(single_data))
+    print(insert_operation(multiple_data))
+    
+    find_operation()
+    delete_operation()
+    update_operation()
